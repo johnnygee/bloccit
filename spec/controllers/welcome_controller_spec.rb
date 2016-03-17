8088640aabf46f0a54407ec6ca1e51ba3fa23068
John Gianakopoulos <gianakopoulos.john@gmail.com>
2016-03-17 14:48:21 -0700

    Assignment 26 complete

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -16,6 +16,13 @@ end
       get :about
       expect(response).to render_template("about")
     end
+  end
+
+  describe "GET faq" do
+    it "renders the faq template" do
+      get :faq
+      expect(response).to render_template("faq")
+    end
   end  
 
 end
