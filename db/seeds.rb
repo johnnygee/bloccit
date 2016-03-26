1781b8fab60e1cc5424cb2ab95ffe7a8d287caba
John Gianakopoulos <gianakopoulos.john@gmail.com>
2016-03-26 15:40:41 -0700

    Assignment 32

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,20 @@ require 'random_data'
    )
  end
 
+ # Create Advertisements
+
+ 5.times do
+
+   Advertisement.create!(
+
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: RandomData.random_number
+   )
+ end
+ advertisements = Advertisement.all
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Advertisement.count} advertisements created"
