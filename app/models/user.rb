e249d698d3d48d1769b0c5516517354f32be4922
John Gianakopoulos <gianakopoulos.john@gmail.com>
2016-04-03 11:19:09 -0700

    formatting changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
  # #3
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
  # #4
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
  # #5
-   validates :email,
+  validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 254 }
