fbbcba1c91f33b759f1da5fe0dd424523227ab50
John Gianakopoulos <gianakopoulos.john@gmail.com>
2016-04-03 11:55:52 -0700

    assignment 36

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_save :format_name
+
 
  # #3
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -14,4 +16,15 @@ class User < ActiveRecord::Base
 
  # #6
    has_secure_password
+
+   def format_name
+     if name
+       name_array = []
+       name.split.each do |name_word|
+         name_array << name_word.capitalize
+       end
+
+       self.name = name_array.join(" ")
+   end
+ end
 end
