3ca1f8ced5b86a628d2fbf84424ffc45b0bb7d24
John Gianakopoulos <gianakopoulos.john@gmail.com>
2016-04-24 22:03:21 -0700

    checkpoint-40

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,11 +40,20 @@ users = User.all
    )
  end
 
- user = User.first
- user.update_attributes!(
-   email: 'gianakopoulos.john@gmail.com', # replace this with your personal email
-   password: 'helloworld'
- )
+ # Create an admin user
+  admin = User.create!(
+    name:     'Admin User',
+    email:    'admin@example.com',
+    password: 'helloworld',
+    role:     'admin'
+  )
+
+  # Create a member
+  member = User.create!(
+    name:     'Member User',
+    email:    'member@example.com',
+    password: 'helloworld'
+  )
 
  puts "Seed finished"
  puts "#{User.count} users created"
