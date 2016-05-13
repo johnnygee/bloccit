fbb0c9cb2abfdc4063ddf811a0f6e4b57aacb12b
John Gianakopoulos <gianakopoulos.john@gmail.com>
2016-05-12 19:17:10 -0700

    revised assingment 40

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -7,8 +7,11 @@ module TopicsHelper
         current_user && current_user.admin?
   end
 
-  def user_is_authorized_for_topics?
+  def user_is_moderator_or_admin?
         current_user && (current_user.admin? || current_user.moderator?)
   end
 
+  def user_is_admin?
+    current_user && current_user.admin?
+  end
 end
