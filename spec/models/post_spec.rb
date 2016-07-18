e5363386068cba8b7263a0f2937b53d5d78ff1cc
John Gianakopoulos <gianakopoulos.john@gmail.com>
2016-07-18 11:12:25 -0700

    checkpoint 45

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:topic) { create(:topic) }
 # #4
   let(:title) { 'My Title' }
   let(:body) { 'Some long body goes here.' }
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-# #2
-  let(:post) { topic.posts.create!(title: title, body: body, user: user) }
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
   it { is_expected.to have_many(:labelings) }
   it { is_expected.to have_many(:labels).through(:labelings) }
 
@@ -29,8 +28,8 @@ RSpec.describe Post, type: :model do
    describe "attributes" do
  # #2
     it "has a title, body, and user attribute" do
-    expect(post).to have_attributes(title: title, body: body, user: user)
-     end
+      expect(post).to have_attributes(title: post.title, body: post.body)
+    end
    end
 
    describe "voting" do
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-
+  let(:topic) { create(:topic) }
   it { is_expected.to have_many(:posts) }
 
   it { is_expected.to have_many(:labelings) }
@@ -12,6 +11,10 @@ RSpec.describe Topic, type: :model do
    describe "attributes" do
      it "responds to name" do
        expect(topic).to respond_to(:name)
+     end
+
+     it "responds to name and description attributes" do
+       expect(topic).to have_attributes(name: topic.name, description: topic.description)
      end
 
      it "responds to description" do
